d8d1ded7dc5aaf9e5d4a2b75ce6473a1b6bd2c73
Schoolkid1 <s.khan@ad-academie.nl>
2023-06-21 12:13:15 +0200

    Administratieve wijzigingen
    Bronvermelding toegevoegd en folder structuur aangepast.

--- Opdracht1/Opdracht1.ino
+++ Opdracht1/Opdracht1.ino
@@ -1,16 +1,20 @@
+//https://brightspace.adainforma.tk/lessons/inf/bp4_hwi_01/#43
+
 //Stroom naar led.
 void setup()
 {
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
 }
-//deleys tussen knipperen (om en om).
+//loop gecreëerd zodat het lichtje aan en uit blijft gaan.
 void loop()
+
 {
   digitalWrite(12, HIGH);
   digitalWrite(11, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
+  //delays tussen knipperen (om en om).
   digitalWrite(12, LOW);
   digitalWrite(11, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
 }
